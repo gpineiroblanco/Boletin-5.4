98a75638888a60e95af7ff22b7be15fbf0c98429
Toshiba <Toshiba@192.168.0.18>
2015-11-02 15:26:56 +0100

    Aplicacion terminada.

---
+++ src/boletin5/pkg4/Boletin54.java
@@ -0,0 +1,19 @@
+
+package boletin5.pkg4;
+
+/**
+ *
+ * @author Toshiba
+ */
+public class Boletin54 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        PulpoConPatatas parida1=new PulpoConPatatas();
+        System.out.println("Clientes: "+parida1.atenderClientes(12, 5));
+    }
+    
+}
